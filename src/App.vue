946f9dd626eff570af1beb929ceed28a2a6ff184
Mingfun Wong <mingfun.wong.chn@gmail.com>
2022-06-02 15:03:24 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" v-bind:key="index">
+          <td><input type="checkbox" :value="item" v-model="selectedItems" /></td>
+          <td>{{ item.Name }}</td>
+          <td>{{ item.Price }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <label><input type="checkbox" v-model="isAllSelected" @click="changeAllSelected()" /> 全选</label>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Goods } from './types/Goods';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+@Component({})
+export default class App extends Vue {
+  items: Goods[] = [
+    {
+      Name: 'Milk',
+      Price: 100,
+    },
+    {
+      Name: 'Cola',
+      Price: 110,
+    },
+    {
+      Name: 'Tapioca',
+      Price: 120,
+    },
+  ];
+  selectedItems: Goods[] = [];
+  isAllSelected = false;
+
+  @Watch('selectedItems')
+  changeSelectedItem() {
+    this.isAllSelected = this.items.length == this.selectedItems.length;
+  }
+
+  changeAllSelected() {
+    if (this.isAllSelected) {
+      this.selectedItems = [];
+    } else {
+      this.selectedItems = this.items;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/types/Goods.ts
@@ -0,0 +1,4 @@
+export interface Goods {
+  Name: string;
+  Price: number;
+}
